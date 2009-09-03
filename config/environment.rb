512511527b7933815d2e4dd5116278b2edb5192d
Dan Nawara <dan@gulliver.(none)>
2009-09-03 16:21:04 -0500

    fixing thoughtbot lib

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'thoughtbot-shoulda', :source => 'http://gems.github.com'
+  config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
