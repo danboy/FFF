e16fe3d58b7d0b3f85ab027414268d7aa8517295
Dan Nawara <dan@zooey.(none)>
2009-09-03 17:13:08 -0500

    floydify script written.

--- public/javascripts/FFF.js
+++ public/javascripts/FFF.js
@@ -1,12 +1,47 @@
-var FFF = {};
+var FFF = function(){
+  this.div = document.createElement('div');
+
+  this.getWindow();
+  this.body = document.getElementsByTagName('body')[0];
+  this.images = [
+  "/images/fff-1.png",
+  "/images/fff-2.png"
+  ];
+
+};
 
 FFF.prototype= {
 
-  foo: function(){
-    alert('foo');
+  floydify: function(){
+    fffSrc=this.images[Math.round(Math.random()*this.images.length)];
+    if ( fffSrc == undefined )
+      fffSrc= this.images[0];
+    var fffImage = document.createElement('img');
+    fffImage.src =  fffSrc;
+    fffImage.style.position = 'absolute';
+    fffImage.style.left = (Math.round( Math.random()*this.windowWidth )-fffImage.width) + 'px';
+    fffImage.style.top = (Math.round( Math.random()*this.windowHeight  ) -fffImage.height)+ 'px';
+
+    this.body.appendChild(fffImage);
   },
-  bar: function(){
-    alert('bar!')
+  getWindow: function(){
+
+	this.windowHeight = 768;
+	this.windowWidth = 1024;
+
+	var de = document.documentElement;
+	if ( typeof(window.innerHeight) == 'number' ) {
+		this.windowHeight = window.innerHeight;
+		this.windowWidth = window.innerWidth;
+	} else if( de && de.clientHeight ) {
+		this.windowHeight = de.clientHeight;
+		this.windowWidth = de.clientWidth;
+	}
+
+  },
+  autoFFF: function(timer){
+  var self = this;
+  setInterval ( function(){self.floydify()}, timer*1000 );
   }
 
 }
